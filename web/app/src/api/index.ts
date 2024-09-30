67974a7f16e9403db67d646afa5495f25454d371
Alex <1353716+alexstojda@users.noreply.github.com>
2024-09-30 17:39:42 -0400

    feat(frontend): Setup vitest with simple test

--- web/app/src/api/index.ts
+++ web/app/src/api/index.ts
@@ -1,1 +1,2 @@
 export * from './api.ts'
+export * from './generated'
--- web/app/vite.config.ts
+++ web/app/vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest/config" />
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app'
+  base: '/app',
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+  }
 })
